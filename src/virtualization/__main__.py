cc51ffeef6cca159ca358252bc37eabbaf59d734
Manjyot Kalkat <57817155+ManjyotK@users.noreply.github.com>
2024-11-12 19:23:15 +0000

    chore: set up github codespace

--- src/virtualization/__main__.py
+++ src/virtualization/__main__.py
@@ -1,12 +1,30 @@
-import sys
-from ._schema import Animation
+# import sys
+# from ._schema import Animation
 
+
+# def main():
+#     print(sys.argv)
+
+#     with open(sys.argv[1]) as f:
+#         data = f.read()
+#     animation = Animation.model_validate_json(data)
+
+#     print(animation)
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def main():
-    print(sys.argv)
+    # Sample data
+    x = [1, 2, 3, 4, 5]
+    y = [5, 6, 2, 3, 13]
+    z = [2, 3, 3, 3, 5]
 
-    with open(sys.argv[1]) as f:
-        data = f.read()
-    animation = Animation.model_validate_json(data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
 
-    print(animation)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
